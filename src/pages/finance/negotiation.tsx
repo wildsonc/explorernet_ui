fe960d1084e40d16922ca6c353c10b4436051f0c
wildsonc <root@dev.local>
2022-05-25 14:29:54 -0300

    0.34.1

--- src/pages/finance/negotiation.tsx
+++ src/pages/finance/negotiation.tsx
@@ -9,27 +9,18 @@ import {
   Text,
   Timeline,
   ScrollArea,
-  Slider,
-  Switch,
   Badge,
   Paper,
   Checkbox,
   createStyles,
   Table,
 } from "@mantine/core";
-import { DatePicker } from "@mantine/dates";
 import { useForm } from "@mantine/form";
 import { showNotification } from "@mantine/notifications";
 import moment from "moment";
 import { useEffect, useMemo, useState } from "react";
 import { useSessionContext } from "supertokens-auth-react/recipe/session";
-import {
-  ArrowRight,
-  Businessplan,
-  FileInvoice,
-  Lock,
-  LockOpen,
-} from "tabler-icons-react";
+import { ArrowRight, FileInvoice, Lock, LockOpen } from "tabler-icons-react";
 import CustumerPainel from "../../components/CustumerPainel";
 import NotAuthorized from "../../components/ErrorPage/NotAuthorized";
 import api from "../../services/api";
@@ -111,54 +102,37 @@ const Negotiation = () => {
   const theme = useMantineTheme();
   const [data, setData] = useState<Props>();
   const [isLoading, setIsLoading] = useState(false);
-  const [usedDays, setUsedDays] = useState(0);
-  const [sliderValue, setSliderValue] = useState(0);
   const [interest, setInterest] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
-  const [nextDate, setNextDate] = useState<Date | null>(new Date());
   const searchForm = useForm({
     initialValues: {
       type: "codconexao",
       value: "",
     },
   });
-  const form = useForm({
-    initialValues: {
-      nextDays: true,
-      aditional: 0,
-      discount: 0,
-    },
-  });
 
   useEffect(() => {
-    const days = dateDiff(nextDate);
     if (!data?.custumer) return;
     let total = 0;
-    total += form.values.aditional;
-    total -= form.values.discount;
-    total += (usedDays * data.custumer.access_plan_price) / 30;
-    if (form.values.nextDays) {
-      total += (days * data.custumer.access_plan_price) / 30;
-    }
     let newInterest = 0;
     data.invoices.forEach((e) => {
       if (selection.includes(e.id)) {
         total += e.value;
         const days = dateDiff(new Date(e.due_date));
-        newInterest += e.value * 0.02 + e.value * days * 0.01;
+        newInterest += e.value * 0.02 + e.value * days * 0.001;
       }
     });
     setInterest(newInterest);
-    setTotalPrice(total);
-  }, [form.values, usedDays, nextDate, selection]);
+    setTotalPrice(total + newInterest);
+  }, [selection]);
 
   const dateDiff = (date: Date | null) => {
     const a = moment(date);
     const days = a.diff(moment().startOf("day"), "days");
     if (days == 0) return 0;
-    if (days < -1) return days * -1 + 1;
+    if (days < -1) return days * -1;
     if (days < 0) return days * -1;
-    return days + 1;
+    return days;
   };
 
   const items = useMemo(() => {
@@ -187,7 +161,7 @@ const Negotiation = () => {
 
   const handleSubmitSearch = async (values: typeof searchForm.values) => {
     setIsLoading(true);
-    resetForm();
+    setSelection([]);
     const response = await api.post<Props>("api/finance/custumer", {
       ...values,
       method: "negotiation",
@@ -200,17 +174,8 @@ const Negotiation = () => {
         color: "red",
       });
     }
-    console.log(response.data);
-    setUsedDays(response.data.used_days);
-    setSliderValue(response.data.used_days);
     setData(response.data);
     setIsLoading(false);
-  };
-
-  const resetForm = () => {
-    form.reset();
-    setSelection([]);
-    setNextDate(new Date());
   };
 
   const toggleRow = (id: string) =>
@@ -219,6 +184,7 @@ const Negotiation = () => {
         ? current.filter((item) => item !== id)
         : [...current, id]
     );
+
   const toggleAll = () => {
     if (!data) return;
     setSelection((current) =>
@@ -317,31 +283,9 @@ const Negotiation = () => {
       <Grid>
         <Grid.Col sm={9}>
           <ScrollArea sx={{ height: 680 }}>
-            <Group mt={-10}>
-              <CustumerPainel {...data} />
-            </Group>
+            <CustumerPainel {...data} />
             {data?.custumer && (
               <>
-                <Text mt={10}>
-                  Dias de uso: <strong>{usedDays}</strong> -{" "}
-                  <strong>
-                    R${" "}
-                    {(
-                      (usedDays * data.custumer.access_plan_price) /
-                      30
-                    ).toFixed(2)}
-                  </strong>
-                </Text>
-                <Slider
-                  value={sliderValue}
-                  onChange={setSliderValue}
-                  onChangeEnd={setUsedDays}
-                  labelAlwaysOn={sliderValue > 6}
-                  color="indigo"
-                  max={31}
-                  size="lg"
-                />
-
                 <Title order={4} mt={10}>
                   Faturas
                 </Title>
@@ -374,104 +318,39 @@ const Negotiation = () => {
                     </Table>
                   </ScrollArea>
                 </Paper>
-                <Group mt={10} position="apart">
-                  <Group>
-                    <Text>Próximo vencimento</Text>
-                    <DatePicker
-                      placeholder="Escolha uma data"
-                      locale="pt-br"
-                      value={nextDate}
-                      onChange={setNextDate}
-                    />
-                    <Text>
-                      {dateDiff(nextDate)}{" "}
-                      {dateDiff(nextDate) > 1 ? "dias" : "dia"}
-                    </Text>
-                    <strong>
-                      R${" "}
-                      {(
-                        (dateDiff(nextDate) * data.custumer.access_plan_price) /
-                        30
-                      ).toFixed(2)}
-                    </strong>
-                    <Switch
-                      {...form.getInputProps("nextDays", {
-                        type: "checkbox",
-                      })}
-                    />
-                  </Group>
-                  <Group>
-                    <TextInput
-                      type="number"
-                      label="Adicional"
-                      icon={<Businessplan size={16} />}
-                      sx={{ width: 100 }}
-                      mt={5}
-                      {...form.getInputProps("aditional")}
-                    />
-                    <TextInput
-                      type="number"
-                      label="Disconto"
-                      icon={<Businessplan size={16} />}
-                      sx={{ width: 100 }}
-                      mt={5}
-                      {...form.getInputProps("discount")}
-                    />
-                  </Group>
-                </Group>
-                <Group mt={3} position="center" align="start">
-                  <Group direction="column" position="center">
-                    <Badge size="lg" color="green" mb={-10}>
-                      Total
-                    </Badge>
-                    <strong>R$ {totalPrice.toFixed(2)}</strong>
-                  </Group>
+                <Group mt={20} position="center" align="start">
                   <Group direction="column" position="center">
                     <Badge size="lg" color="lime" mb={-10}>
                       À vista
                     </Badge>
                     <Text weight={500}>
-                      R$ {(totalPrice * 0.95).toFixed(2)}
-                    </Text>
-                    <Text mt={-20} color="green" weight={700}>
-                      - {(totalPrice * 0.05).toFixed(2)}
+                      R$ {(totalPrice - interest).toFixed(2)}
                     </Text>
                   </Group>
                   <Group direction="column" position="center">
                     <Badge size="lg" color="grape" mb={-10}>
-                      2X
+                      3X
                     </Badge>
                     <Text weight={500}>R$ {totalPrice.toFixed(2)}</Text>
                     <Text mt={-20} color="dimmed" weight={500}>
-                      2x {(totalPrice * 0.5).toFixed(2)}
+                      3x {(totalPrice / 3).toFixed(2)}
+                    </Text>
+                    <Text mt={-20} color="red" weight={700}>
+                      + {interest.toFixed(2)}
                     </Text>
                   </Group>
                   <Group direction="column" position="center">
                     <Badge size="lg" color="indigo" mb={-10}>
-                      3X
+                      6X
                     </Badge>
-                    <Text weight={500}>R$ {totalPrice.toFixed(2)}</Text>
+                    <Text weight={500}>
+                      R$ {(totalPrice * 1.12).toFixed(2)}
+                    </Text>
                     <Text mt={-20} weight={500} color="dimmed">
-                      3x {(totalPrice / 3).toFixed(2)}
+                      6x {((totalPrice * 1.12) / 6).toFixed(2)}
                     </Text>
-                  </Group>
-                  <Group direction="column" position="center">
-                    <Badge size="lg" color="red" mb={-10}>
-                      1+2X
-                    </Badge>
-                    <Group>
-                      <Text weight={500}>
-                        R$ {(interest + totalPrice).toFixed(2)}
-                      </Text>
-                      <Text color="red" weight={700}>
-                        + {interest.toFixed(2)}
-                      </Text>
-                    </Group>
-                    <Text mt={-20} color="dimmed" weight={500}>
-                      Entrada {((interest + totalPrice) * 0.4).toFixed(2)}
-                    </Text>
-                    <Text mt={-20} color="dimmed" weight={500}>
-                      2x {(((interest + totalPrice) * 0.6) / 2).toFixed(2)}
+                    <Text mt={-20} color="red" weight={700}>
+                      + {(totalPrice * 1.12 - totalPrice).toFixed(2)}
                     </Text>
                   </Group>
                 </Group>
